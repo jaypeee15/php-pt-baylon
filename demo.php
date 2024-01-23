0550a9a9229ae544f9ecdf947134e48849854a42
unknown <johnpatrickbreis@gmail.com>
2024-01-22 18:32:49 -0800

    New File

---
+++ demo.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Simple form processing</title>
+</head>
+
+<body>
+    <form action="form_1.php"method="post">
+        FirstName:
+        <input type="text"name="firtname" required/>
+        <br>
+        <br>
+        LastName:
+        <input type="text"name="lastname" required/>
+        <br>
+        <br>
+        Address 
+        <input type="text"name="address" required/>
+        <br>
+        <br>
+        Email Address:
+        <input type="text"name="lastname" required/>
+        <br>
+        <br>
+        Password:
+        <input type="text"name="password" required/>
+        <br>
+        <br>
+        <input type="submit" value=Sumbit>
+</form>
+</body>
+</html>
+
